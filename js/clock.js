9d84f396f35b1033f0c3d5ad6dbdff8fa82e644c
rostami2001 <123289362+rostami2001@users.noreply.github.com>
2024-08-20 10:51:37 +0330

    Add files via upload

---
+++ js/clock.js
@@ -0,0 +1,52 @@
+function clock() {
+    let time = new Date();
+
+    let Hours = time.getHours();
+    let Minutes = time.getMinutes();
+    let Seconds = time.getSeconds();
+
+    let typeOfTime = Hours >= 12 ? 'PM' : 'AM';
+
+    Hours = Hours % 12;
+    if (Hours === 0) {
+        Hours = 12;
+    }
+    Minutes = Minutes < 10 ? '0' + Minutes : Minutes;
+    Seconds = Seconds < 10 ? '0' + Seconds : Seconds;
+
+    let currentTime = Hours + ':' + Minutes + ':' + Seconds + ' ' + typeOfTime;
+
+    document.getElementById('clock').textContent = currentTime
+}
+
+function randomCordinate() {
+    let pos = document.getElementById('current-time');
+
+    let clockWidth = pos.offsetWidth;
+    let clockHeight = pos.offsetHeight;
+
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+
+    width = width - clockWidth;
+    height = height - clockHeight;
+
+    let x = Math.floor(Math.random() * width);
+    let y = Math.floor(Math.random() * height);
+
+    console.log('Random Coordinates:', x, y);
+
+    move(x, y);
+}
+
+function move(x , y) {
+    let pos = document.getElementById('current-time');
+
+    pos.style.left = x +'px';
+    pos.style.top = y + 'px';
+}
+
+clock();
+setInterval(clock, 1000);
+setInterval(randomCordinate, 5000);
+
